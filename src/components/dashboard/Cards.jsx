6837f0ee725f8ded0de66204959382c685c64e8b
Faishal <ishal.2401@gmail.com>
2024-06-09 11:07:03 +0700

    Delete 1 line

--- src/components/dashboard/Cards.jsx
+++ src/components/dashboard/Cards.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import { GoPeople } from "react-icons/go";
 import { AiOutlineCalendar, AiOutlineClockCircle } from "react-icons/ai";
 import { MdOutlineAssignmentLate } from "react-icons/md";
