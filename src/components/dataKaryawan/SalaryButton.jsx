c13f08445e3cb55896030616ad30fff1c5deafb3
Faishal <ishal.2401@gmail.com>
2024-10-08 15:53:16 +0700

    Add reset salary button

--- src/components/dataKaryawan/SalaryButton.jsx
+++ src/components/dataKaryawan/SalaryButton.jsx
@@ -18,18 +18,41 @@ const SalaryButton = () => {
           month: new Date().getMonth() + 1,
         }
       );
+
+      alert("Proses gaji berhasil!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleReset = async () => {
+    try {
+      await axios.put(
+        `${import.meta.env.VITE_IP_ADDRESS}/api/admin/reset-salary`
+      );
+
+      alert("Reset Berhasil");
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <button
-      className="py-1 px-2 bg-orange-500 text-white rounded-lg"
-      onClick={handleClick}
-    >
-      Proses Gaji
-    </button>
+    <div className="flex gap-5">
+      <button
+        className="py-1 px-2 bg-sky-500 text-white rounded-lg"
+        onClick={handleClick}
+      >
+        Proses Gaji
+      </button>
+
+      <button
+        className="py-1 px-2 bg-rose-500 text-white rounded-lg"
+        onClick={handleReset}
+      >
+        Reset Gaji
+      </button>
+    </div>
   );
 };
 
